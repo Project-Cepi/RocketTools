36649ac72fcf740c756333616144819c858cd276
LeoDog896 <LeoDog896@hotmail.com>
2020-12-10 12:26:36 -0500

    Bump minestom

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,7 +29,7 @@ dependencies {
     implementation(kotlin("reflect"))
 
     // Compile Minestom into project
-    implementation("com.github.Minestom:Minestom:d61a598c94")
+    implementation("com.github.Minestom:Minestom:62ab0a2007")
 }
 
 tasks.withType<Test> {
