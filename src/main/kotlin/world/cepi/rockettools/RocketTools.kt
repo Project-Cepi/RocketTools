8e340acbf0284fc495d2771607af25024e06208d
benjables <59124306+benjables@users.noreply.github.com>
2020-12-09 21:23:26 -0500

    added /extensions (tab completion probs coming soon)

--- src/main/kotlin/world/cepi/rockettools/RocketTools.kt
+++ src/main/kotlin/world/cepi/rockettools/RocketTools.kt
@@ -2,6 +2,7 @@ package world.cepi.rockettools
 
 import net.minestom.server.MinecraftServer
 import net.minestom.server.extensions.Extension;
+import world.cepi.rockettools.command.ExtensionsCommand
 import world.cepi.rockettools.command.RocketCommand
 
 class RocketTools : Extension() {
@@ -9,7 +10,7 @@ class RocketTools : Extension() {
     override fun initialize() {
 
         MinecraftServer.getCommandManager().register(RocketCommand())
-
+        MinecraftServer.getCommandManager().register(ExtensionsCommand())
         logger.info("[RocketTools] has been enabled!")
     }
 
--- src/main/kotlin/world/cepi/rockettools/command/RocketCommand.kt
+++ src/main/kotlin/world/cepi/rockettools/command/RocketCommand.kt
@@ -1,6 +1,11 @@
 package world.cepi.rockettools.command
 
+import com.extollit.misc.RGB.GREEN
+import net.minestom.server.MinecraftServer
+import net.minestom.server.chat.ChatColor
 import net.minestom.server.command.builder.Command
+import net.minestom.server.command.builder.arguments.ArgumentType
+import net.minestom.server.entity.Player
 
 class RocketCommand : Command("rocket") {
 
@@ -12,7 +17,17 @@ class RocketCommand : Command("rocket") {
      */
 
     init {
-
+        val arg1 = ArgumentType.Word("arg1")
+        val arg2 = ArgumentType.Word("arg2")
+        addSyntax({sender, args ->
+            val player = sender as Player
+            val extension = MinecraftServer.getExtensionManager().getExtension(args.getWord("arg2"))
+            if (extension != null) {
+                if (args.getWord("arg1") == "reload")
+                //MinecraftServer.getExtensionManager().reload(args.getWord("arg2"))
+                player.sendMessage("${ChatColor.BRIGHT_GREEN}Extension reloaded!")
+            }
+        }, arg1, arg2)
     }
 
 }
