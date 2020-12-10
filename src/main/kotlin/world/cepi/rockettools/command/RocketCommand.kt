fb675bda2527452955fdd26aaafc0f4215820126
benjables <59124306+benjables@users.noreply.github.com>
2020-12-09 21:51:46 -0500

    added tab completion to command!

--- src/main/kotlin/world/cepi/rockettools/command/RocketCommand.kt
+++ src/main/kotlin/world/cepi/rockettools/command/RocketCommand.kt
@@ -4,8 +4,13 @@ import com.extollit.misc.RGB.GREEN
 import net.minestom.server.MinecraftServer
 import net.minestom.server.chat.ChatColor
 import net.minestom.server.command.builder.Command
+import net.minestom.server.command.builder.arguments.ArgumentDynamicWord
 import net.minestom.server.command.builder.arguments.ArgumentType
+import net.minestom.server.command.builder.arguments.ArgumentType.DynamicWord
+import net.minestom.server.command.builder.arguments.ArgumentType.Word
 import net.minestom.server.entity.Player
+import javax.annotation.Nullable
+import javax.print.DocFlavor
 
 class RocketCommand : Command("rocket") {
 
@@ -18,7 +23,7 @@ class RocketCommand : Command("rocket") {
 
     init {
         val arg1 = ArgumentType.Word("arg1")
-        val arg2 = ArgumentType.Word("arg2")
+        val arg2 = ArgumentType.DynamicWord("extension")
         addSyntax({sender, args ->
             val player = sender as Player
             val extension = MinecraftServer.getExtensionManager().getExtension(args.getWord("arg2"))
@@ -28,6 +33,15 @@ class RocketCommand : Command("rocket") {
                 player.sendMessage("${ChatColor.BRIGHT_GREEN}Extension reloaded!")
             }
         }, arg1, arg2)
+    }
+    @Nullable
+    override fun onDynamicWrite(text: String): Array<String> {
+        var array = mutableListOf<String>()
+        val list = MinecraftServer.getExtensionManager().extensions
+        for (bruh in list) {
+            array.add(bruh.description.name)
+        }
+        return array.toTypedArray()
     }
 
 }
