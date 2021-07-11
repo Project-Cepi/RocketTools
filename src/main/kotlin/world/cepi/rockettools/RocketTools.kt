63a5444976b27a6b4ca9f6b5707f36b40cea9713
LeoDog896 <LeoDog896@hotmail.com>
2021-07-11 11:11:28 -0400

    Begin basic /command

--- src/main/kotlin/world/cepi/rockettools/RocketTools.kt
+++ src/main/kotlin/world/cepi/rockettools/RocketTools.kt
@@ -3,17 +3,22 @@ package world.cepi.rockettools
 import net.minestom.server.extensions.Extension;
 import world.cepi.kstom.command.register
 import world.cepi.kstom.command.unregister
+import world.cepi.rockettools.command.CommandViewer
 import world.cepi.rockettools.command.RocketCommand
 
 class RocketTools : Extension() {
 
     override fun initialize() {
+        CommandViewer.register()
         RocketCommand.register()
+
         logger.info("[RocketTools] has been enabled!")
     }
 
     override fun terminate() {
+        CommandViewer.unregister()
         RocketCommand.unregister()
+
         logger.info("[RocketTools] has been disabled!")
     }
 
